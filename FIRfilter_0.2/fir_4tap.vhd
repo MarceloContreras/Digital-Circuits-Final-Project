16864d1549ac2795a2c8e5552fb95000c5d0c662
MarceloContreras <51939884+MarceloContreras@users.noreply.github.com>
2021-07-20 22:47:32 -0500

    Update fir_4tap.vhd

--- FIRfilter_0.2/fir_4tap.vhd
+++ FIRfilter_0.2/fir_4tap.vhd
@@ -99,42 +99,5 @@ end process;
    
 end Behavioral;
 
---filter coefficient initializations.
---H = [-2 -1 3 4].
-H0 <= X"01";
-H1 <= X"02";
-H2 <= X"03";
-H3 <= X"02";
-
---Multiple constant multiplications.
---MCM3 <= H3*Xi;
-Mult3: Wallace8 port map(H3,Xin,MCM3);
-Mult2: Wallace8 port map(H2,Xin,MCM2);
-Mult1: Wallace8 port map(H1,Xin,MCM1);
-Mult0: Wallace8 port map(H0,Xin,MCM0);
-
---adders
-
-sum1<=unsigned(Q1)+unsigned(MCM2);
-sum2<=unsigned(Q2)+unsigned(MCM1);
-sum3<=unsigned(Q3)+unsigned(MCM0);
-
-add_out1<=std_logic_vector(sum1);
-add_out2<=std_logic_vector(sum2);
-add_out3<=std_logic_vector(sum3);
 
 
---flipflops(for introducing a delay).
-dff1 : DFF port map(Q1,Clk,MCM3);
-dff2 : DFF port map(Q2,Clk,add_out1);
-dff3 : DFF port map(Q3,Clk,add_out2);
-
---an output produced at every positive edge of clock cycle.
-process(Clk)
-begin
-    if(rising_edge(Clk)) then
-        Yout <= add_out3;
-    end if;
-end process;
-   
-end Behavioral;
