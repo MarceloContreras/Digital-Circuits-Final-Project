01492f58751d35d2a0f6e4b237ae7c8511d302b4
MarceloContreras <51939884+MarceloContreras@users.noreply.github.com>
2021-07-21 15:09:31 -0500

    Update sum_booth.vhd

--- FIRfilter_0.2/sum_booth.vhd
+++ FIRfilter_0.2/sum_booth.vhd
@@ -4,12 +4,11 @@ use ieee.std_logic_signed.ALL;
 
 entity booth_multiplier is
 
-	GENERIC (x : INTEGER := 16;
-		 y : INTEGER := 16);
+	GENERIC (L : INTEGER := 16);
 	
-	PORT(m : IN std_logic_vector(x - 1 downto 0);
-	     r : IN std_logic_vector(y - 1 downto 0);
-	     prod : OUT std_logic_vector(x + y - 1 downto 0));
+	PORT(m : IN std_logic_vector(L- 1 downto 0);
+	     r : IN std_logic_vector(L - 1 downto 0);
+	     prod : OUT std_logic_vector(2*L - 1 downto 0));
 		  
 end booth_multiplier;
 
@@ -19,11 +18,11 @@ begin
 	
 process(m, r)
 
-	constant X_ZEROS : std_logic_vector(x - 1 downto 0) := (others => '0');
-	constant Y_ZEROS : std_logic_vector(y - 1 downto 0) := (others => '0');
+	constant X_ZEROS : std_logic_vector(L - 1 downto 0) := (others => '0');
+	constant Y_ZEROS : std_logic_vector(L - 1 downto 0) := (others => '0');
 
-	variable a, s, p : std_logic_vector(x + y + 1 downto 0);
-	variable mn      : std_logic_vector(x - 1 downto 0);
+	variable a, s, p : std_logic_vector(2*L + 1 downto 0);
+	variable mn      : std_logic_vector(L - 1 downto 0);
 
 begin
 	
@@ -35,19 +34,19 @@ begin
 -- Conteo de operaciones restantes y complemento
 	if (m /= X_ZEROS AND r /= Y_ZEROS) then
 
-		a(x + y downto y + 1) := m;
-		a(x + y + 1) := m(x - 1);
+		a(2*L downto L + 1) := m;
+		a(2*L + 1) := m(L - 1);
 
 		mn := (not m) + 1;
 
-		s(x + y downto y + 1) := mn;
-		s(x + y + 1) := NOT(m(x - 1));
+		s(2*L downto L + 1) := mn;
+		s(2*L + 1) := NOT(m(L - 1));
 
-		p(y downto 1) := r;
+		p(L downto 1) := r;
 
 -- Operación suma o resta
 
-		for i in 1 to y loop
+		for i in 1 to L loop
 
 			if (p(1 downto 0) = "01") then
 				p := p + a;
@@ -56,13 +55,13 @@ begin
 			end if;
 
 -- Shifting
-			p(x + y downto 0) := p(x + y + 1 downto 1);
+			p(2*L downto 0) := p(2*L + 1 downto 1);
 
 		end loop;
 
 	end if;
 
-	prod <= p(x + y downto 1);
+	prod <= p(2*L downto 1);
 
 end process;
 	
